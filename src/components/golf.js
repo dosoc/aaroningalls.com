376b8adce46908b60b290ca788717951a7f1b86e
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-08-22 16:08:31 -0500

    wip

--- src/components/golf.js
+++ src/components/golf.js
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+const initialFormData = {
+    year: '',
+    tournament: '',
+    scores: '',
+    position: ''
+}
 
 const Golf = () => {
+    const [results, setResults] = useState([])
+    const [formData, setFormData] = useState(initialFormData)
+
+    const setForm = (evt) => {
+        const { name, value} = evt.target
+        setFormData({
+            ...formData,
+            [name] : value
+        })
+    }
+
+    const addResults = (e) => {
+        e.preventDefault(e)
+        setResults([
+            ...results,
+            formData
+        ])
+
+        
+    }
+    
+
     return (
         <div className='container'>
             <div className='title'>
@@ -9,15 +38,55 @@ const Golf = () => {
                 I am using this as a blog/tracker to keep track of my results</p>
             </div>
             <div>
-                <form>
-                    <p>Insert search form here</p>
+                <form onSubmit={addResults}>
+                    <input
+                        type='number'
+                        name='year'
+                        onChange ={setForm}
+                        value={formData.year}
+                    />
+                    <input
+                        type='text'
+                        name='tournament'
+                        onChange ={setForm}
+                        value={formData.tournament}
+                    />
+                    <input
+                        type='text'
+                        name='scores'
+                        onChange ={setForm}
+                        value={formData.scores}
+                    />
+                    <input
+                        type='text'
+                        name='position'
+                        onChange ={setForm}
+                        value={formData.position}
+                    />
+                    <button type='submit'>Add Results</button>
                 </form>
                 <table>
                     <thead>
-                        <tr><th>Insert table here</th></tr>
+                        <tr>
+                            <th>Year</th>
+                            <th>Tournament</th>
+                            <th>Scores</th>
+                            <th>Position</th>
+                        </tr>
                     </thead>
                     <tbody>
-                        <tr><td>results</td></tr>
+                        {!results?<p>Enter results</p>: results.map((each, key) => {
+                            each['id'] = key
+                            return(
+                                 <tr key={key}>
+                                    <td>{each.year}</td>
+                                    <td>{each.tournament}</td>
+                                    <td>{each.scores}</td>
+                                    <td>{each.position}</td>
+                                </tr>
+                                )
+                        })}
+                        
                     </tbody>
                 </table>
             </div>
